69e827e87982a579aa861ed432167007f93d5749
implank <>
2022-03-24 15:25:18 +0800

    lab1-1-Extra(7)

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -85,16 +85,16 @@ int readelf(u_char *binary, int size)
 			//		printf("Overlay at page va : 0x%x\n",l1);
 			//		printf("0x%x 0x%x 0x%x\n",r1,l2,r2);
 			//		return 0;
-					if(l1<ans1)
-							ans1=l1,f=0;
+					if(round1<ans1)
+							ans1=round1,f=0;
 					
 				}
 				else {
 				//	printf("Conflict at page va : 0x%x\n",l1);
 			//		printf("0x%x 0x%x 0x%x\n",r1,l2,r2);
 			//		return 0;
-					if(l1<ans1)
-							ans1=l1,f=1;
+					if(round1<ans1)
+							ans1=round1,f=1;
 				}
 			}
 		}	
