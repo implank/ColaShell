7c6d6abe899205ca449e597dda5768cc60d267ce
implank <>
2022-03-24 14:11:41 +0800

    lab1-1-exam(1)

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -69,9 +69,9 @@ int readelf(u_char *binary, int size)
 	
 	// for each section header, output section number and section addr. 
 	// hint: section number starts at 0.
-	for(Nr=0;Nr<sh_entry_count;Nr++){
+	for(Nr=0;Nr<sh_entry_count;Nr++)if(Nr==2||Nr==3){
 		shdr=(Elf32_Shdr *)(ptr_sh_table+Nr*sh_entry_size);
-		printf("%d:0x%x\n",Nr,shdr->sh_addr);
+		printf("Read:%d:0x%x,0x%x\n",Nr,shdr->sh_offset,shdr->sh_addr);
 	}
 
 	return 0;
