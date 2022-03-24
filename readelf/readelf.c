af0713b9fa118420adec19bad6128c4bc5da31fb
implank <>
2022-03-24 15:29:02 +0800

    lab1-1-Extra(8)

--- readelf/readelf.c
+++ readelf/readelf.c
@@ -70,7 +70,7 @@ int readelf(u_char *binary, int size)
 	// for each section header, output section number and section addr. 
 	// hint: section number starts at 0.
 	phdr=(Elf32_Phdr *)ptr_ph_table;
-	int f=-1;unsigned ans1=0xffffffff;
+	int f=-1;int ans1=0xffffff;
 	for(Nr=0;Nr<ph_entry_count;Nr++){
 		Elf32_Phdr *p1=phdr+Nr;
 		int l1=p1->p_vaddr;
