b4c9c5fdc3b4ca9d20f0289396c9e4a9fdd975fa
implank <>
2022-03-31 15:57:28 +0800

    for cal

--- my_cal/my_cal.c
+++ my_cal/my_cal.c
@@ -1,5 +1,21 @@
 void my_cal(){
-	char c=_my_getchar();
-	_my_putchar(c);
-	return ;
+	long long num=0;
+	char c;
+	while(1){
+		c=_my_getchar();
+		if('0'<=c&&c<='9');
+		else break;
+		num=num*10+c-'0';
+	}
+	//printf("%d\n",num);
+	int i=0,f=0;
+	for(i=31;i>=0;--i){
+		if((num>>i)&1){
+			_my_putchar('1');
+			f=1;
+		}
+		else if(f)
+			_my_putchar('0');
+	}
+		return ;
 }
